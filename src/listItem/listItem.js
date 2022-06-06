54aaddd1887cfbe39b4ddd69676564d374127cd7
Arsenii Kopaev <senya.kopaev@mail.ru>
2022-06-06 10:19:42 +0300

    new design

--- src/listItem/listItem.js
+++ src/listItem/listItem.js
@@ -1,14 +1,26 @@
+import { BsFillXCircleFill, BsFillBookmarkFill } from "react-icons/bs";
+
 import './listItem.css'
 
 const ListItem = ({onDeleteTask, onSpecialTask, item}) => {
     const {id, text} = item;
-    return (
-        <div className='task-list__item' key={id}>
-            <span className='task-list__item-text'>{text}</span>
-            <button className='task-list__item-button' onClick={() => onDeleteTask(id)}>delete</button>
-            <button className='task-list__item-button' onClick={() => onSpecialTask(id)}>special</button>
-        </div>
-    )
+    if(item.special){
+        return (
+            <div className='task-list__item' key={id}>
+                <span className='task-list__item-special-text'>{text}</span>
+                <button className='task-list__item-button-delete' onClick={() => onDeleteTask(id)}><BsFillXCircleFill /></button>
+                <button className='task-list__item-button-special' onClick={() => onSpecialTask(id)}><BsFillBookmarkFill /></button>
+            </div>
+        )
+    } else {
+        return (
+            <div className='task-list__item' key={id}>
+                <span className='task-list__item-text'>{text}</span>
+                <button className='task-list__item-button-delete' onClick={() => onDeleteTask(id)}><BsFillXCircleFill /></button>
+                <button className='task-list__item-button-special' onClick={() => onSpecialTask(id)}><BsFillBookmarkFill /></button>
+            </div>
+        )
+    }   
 }
 
 export default ListItem;
