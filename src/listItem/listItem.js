a2eb6dc403d0ca85107c1903ba35ebd3a4e1b839
Arsenii Kopaev <senya.kopaev@mail.ru>
2022-06-06 12:52:34 +0300

    list item was optimized

--- src/listItem/listItem.js
+++ src/listItem/listItem.js
@@ -3,24 +3,16 @@ import { BsFillXCircleFill, BsFillBookmarkFill } from "react-icons/bs";
 import './listItem.css'
 
 const ListItem = ({onDeleteTask, onSpecialTask, item}) => {
-    const {id, text} = item;
-    if(item.special){
-        return (
-            <div className='task-list__item' key={id}>
-                <span className='task-list__item-special-text'>{text}</span>
-                <button className='task-list__item-button-delete' onClick={() => onDeleteTask(id)}><BsFillXCircleFill /></button>
-                <button className='task-list__item-button-special' onClick={() => onSpecialTask(id)}><BsFillBookmarkFill /></button>
-            </div>
-        )
-    } else {
-        return (
-            <div className='task-list__item' key={id}>
-                <span className='task-list__item-text'>{text}</span>
-                <button className='task-list__item-button-delete' onClick={() => onDeleteTask(id)}><BsFillXCircleFill /></button>
-                <button className='task-list__item-button-special' onClick={() => onSpecialTask(id)}><BsFillBookmarkFill /></button>
-            </div>
-        )
-    }   
+    const {id, text, special} = item;
+    let textCondition = 'task-list__item-text';
+    if (special) textCondition = `${textCondition} special`;
+    return (
+        <div className='task-list__item' key={id}>
+            <span className={textCondition}>{text}</span>
+            <button className='task-list__item-button-delete' onClick={() => onDeleteTask(id)}><BsFillXCircleFill /></button>
+            <button className='task-list__item-button-special' onClick={() => onSpecialTask(id)}><BsFillBookmarkFill /></button>
+        </div>
+    )
 }
 
 export default ListItem;
